716f7bc248361561f101fea1d0ea5d86dce6a425
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-28 18:16:49 -0500

    chore: initial configuration of Next.js project with TypeScript, Tailwind CSS, React Query, ESLint (React and React Hooks), and Prettier all set up in one go.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+import eslint_plugin_react from 'eslint-plugin-react';
+import eslint_plugin_react_hooks from 'eslint-plugin-react-hooks';
+import eslint_plugin_prettier from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react: eslint_plugin_react,
+      'react-hooks': eslint_plugin_react_hooks,
+      prettier: eslint_plugin_prettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
